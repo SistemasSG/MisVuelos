c7d64fc34bf81d2ed331b4ec357d78aaa4747171
Jesus Parra <sistemas@stargas.com.ve>
2017-11-04 15:44:08 -0400

    Revert "Reservas 2"
    This reverts commit fe94d3005174b1e0875d638fcc21f2fc21b3441e.

---
+++ MisVuelos/MisVuelos/ViewModels/ListaVuelosViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisVuelos.ViewModels
+{
+    public class ListaVuelosViewModel
+    {
+        public ListaVuelosViewModel()
+        {
+            //CargarVuelos();
+        }
+
+        //public ObservableCollection<Models.Vuelos> ListaVuelos
+        //{
+        //    get;
+        //    set;
+        //}
+
+        //private async void CargarVuelos()
+        //{
+        //    try
+        //    {
+        //        ListaVuelos = new ObservableCollection<Models.Vuelos>();
+        //        var client = await App.Database.GetVuelosAsync();
+        //        var lista = client.ToList();
+
+        //        foreach (var item in lista)
+        //        {
+        //            ListaVuelos.Add(item);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //    }
+        //}
+
+    }
+}
--- MisVuelos/MisVuelos/ViewModels/MainPageViewModel.cs
+++ MisVuelos/MisVuelos/ViewModels/MainPageViewModel.cs
@@ -33,7 +33,6 @@ namespace MisVuelos.ViewModels
             {
                 ListaAerolineas = App.Database.GetAerolineas().ToList();
                 ListaCiudades = App.Database.GetCiudades().ToList();
-
             }
             catch (Exception ex)
             {
